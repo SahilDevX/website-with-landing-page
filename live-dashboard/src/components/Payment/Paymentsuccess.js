d81b582162f0633fabeb9920789701c3072c2c6b
SahilDevX <sahildange7057@gmail.com>
2024-12-20 23:01:32 +0530

    Payment Gateway-2

--- live-dashboard/src/components/Payment/Paymentsuccess.js
+++ live-dashboard/src/components/Payment/Paymentsuccess.js
@@ -1,8 +1,13 @@
 import React from 'react'
 
-export default function () {
+export default function paymentsuccess() {
   return (
     <div>
+      <h1>Success</h1>
+      <h1>Success</h1>
+      <h1>Success</h1>
+      <h1>Success</h1>
+      <h1>Success</h1>
       <h1>Success</h1>
       <h1>Success</h1>
       <h1>Success</h1>
